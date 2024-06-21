1a5a5ca3e493b63b7df4b8b1292e7f2f28224317
MohdSaeed <saeed@capitalnumbers.com>
2024-06-21 19:17:05 +0530

    Subsequence

---
+++ Array/Subsequence/Solution.cs
@@ -0,0 +1,23 @@
+public class Solution {
+    public bool IsSubsequence(string s, string t) {
+        int i = 0, j = 0;
+        while(i < s.Length && j < t.Length)
+        {
+            if(s[i] == t[j])
+            {
+                i++;
+                
+            }
+            j++;
+            
+        }
+
+        return (i== s.Length);
+
+    }
+
+    static void Main(string[] args)
+    {
+        Console.WriteLine(IsSubsequence("adhjst", "ahs"));
+    }
+}
