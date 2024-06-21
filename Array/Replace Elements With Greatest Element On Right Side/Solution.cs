ebe42d3f193211dadb40c3eae0bb75066f24c978
MohdSaeed <saeed@capitalnumbers.com>
2024-06-21 19:02:14 +0530

    Replace Element

---
+++ Array/Replace Elements With Greatest Element On Right Side/Solution.cs
@@ -0,0 +1,18 @@
+public class Solution {
+    public static int[] ReplaceElements(int[] arr) {
+       int max = -1;
+       for(int j = arr.Length -1; j >= 0; j--)
+       {
+           int newMax = arr[j] > max ? arr[j] : max;
+           arr[j] = max;
+           max = newMax;
+           
+       }
+
+       return arr;
+    }
+
+      int[] array = { 12, 11, 13, 5, 6, 7, 100, 45, 80, 200, 132, 111 };
+      Console.WriteLine(ReplaceElements(array));
+}
+
